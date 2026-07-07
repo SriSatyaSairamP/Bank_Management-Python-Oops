bd766187918c528bc20445793186e84e7c09918c
SriSatyaSairamP <ai.odyssey20@gmail.com>
2026-07-07 19:22:15 +0530

    feat:Add automatic account number generation

--- Bank-Management-Python-Oops.py
+++ Bank-Management-Python-Oops.py
@@ -1,9 +1,11 @@
 from abc import ABC,abstractmethod
 
 class Account(ABC):
-
+    next_account_number= 10001
     
     def __init__(self,name,balance):
+        self.account_number = Account.next_account_number
+        Account.next_account_number +=1
         self.name=name
         self._validate_amount(balance)
         self.transactions =[]
@@ -27,14 +29,15 @@ class Account(ABC):
         new_balance=self.get_balance()+amount
         self.set_balance(new_balance)
         self.transactions.append(f"+₹{amount} Deposited")
-        print(f"₹{amount} deposited successfully")
+        print(f"{self.name} (Account No: {self.account_number}) deposited ₹{amount} successfully. Current Balance: ₹{self.get_balance()}")
 
     @abstractmethod
     def withdraw(self,amount):
         pass
 
     def display(self):
-        print(f"\nAccount Holder: {self.name}")
+        print(f"\nAccount NUmber: {self.account_number}")
+        print(f"Account Holder: {self.name}")
         print(f"Balance: {self.get_balance()}")
 
     def _deduct(self,amount):
@@ -42,9 +45,11 @@ class Account(ABC):
         self.set_balance(new_balance)
 
     def show_transactions(self):
-        print(f"\nTransaction History - {self.name}")
+        print(f"\nTransaction History")
+        print(f"Account Number: {self.account_number}")
+        print(f"Account Holder: {self.name}")
         if not self.transactions:
-            print("No transcations found")
+            print("No transactions found")
         else:
             for transaction in self.transactions:
                 print(transaction)
@@ -61,7 +66,7 @@ class SavingsAccount(Account):
         else:
             self._deduct(amount)
             self.transactions.append(f"-₹{amount} Withdrawn")
-            print(f"₹{amount} withdrawn successfully")
+            print(f"Withdrawal of ₹{amount} from {self.name}'s account (Account No: {self.account_number}) was successful. Current Balance: ₹{self.get_balance()}")
 
 
 class CurrentAccount(Account):
@@ -73,7 +78,7 @@ class CurrentAccount(Account):
         else:
             self._deduct(amount)
             self.transactions.append(f"-₹{amount} Withdrawn")
-            print(f"₹{amount} withdrawn successfully")
+            print(f"Withdrawal of ₹{amount} from {self.name}'s account (Account No: {self.account_number}) was successful. Current Balance: ₹{self.get_balance()}")
 
 class Bank:
 
