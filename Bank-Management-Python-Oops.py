5b94a05bbabf2f7cc99860f1c308a29cb846c8f6
SriSatyaSairamP <ai.odyssey20@gmail.com>
2026-07-09 19:43:10 +0530

    feat: redesign Bank Project

--- Bank-Management-Python-Oops.py
+++ Bank-Management-Python-Oops.py
@@ -1,21 +1,70 @@
 from abc import ABC,abstractmethod
 
+class Bank:
+    bank_name = "Python Bank"
+    accounts=[]
+    savings_accounts=[]
+    current_accounts=[]
+
+    @classmethod
+    def register_account(cls,account):
+        cls.accounts.append(account)
+
+        if isinstance(account,SavingsAccount):
+            cls.savings_accounts.append(account)
+        elif isinstance(account,CurrentAccount):
+            cls.current_accounts.append(account)
+
+    @classmethod
+    def display_all_accounts(cls):
+        print(f"\nAll Accounts in {cls.bank_name}")
+
+        if not cls.accounts:
+            print("No accounts have been registered yet.")
+        else:
+            for account in cls.accounts:
+                account.display()
+
+    @classmethod
+    def display_total_balance(cls):
+        total=0
+        for account in cls.accounts:
+            total+=account.get_balance()
+        print(f"\nTotal Bank Balance: ₹{total:.2f}")
+
+
 class Account(ABC):
     next_account_number= 10001
     
-    def __init__(self,name,balance):
+    def __init__(self,name,branch_name,balance):
         self.account_number = Account.next_account_number
         Account.next_account_number +=1
         self.name=name
+        self.branch_name=branch_name
         self._validate_amount(balance)
+        self.__balance = float(balance)
         self.transactions =[]
-        self.__balance=float(balance)
+
+        self.transactions.append(f"Initial Deposit: +₹{balance}")
+
+        Bank.register_account(self)
+
+        print(
+            f"\n{self.__class__.__name__} created successfully"
+            f"\nBank Name       : {Bank.bank_name}"
+            f"\nBranch Name     : {self.branch_name}"
+            f"\nAccount Number  : {self.account_number}"
+            f"\nAccount Holder  : {self.name}"
+            f"\nInitial Deposit : ₹{self.get_balance():.2f}"
+
+        )
+
 
     def _validate_amount(self,amount):
         if not isinstance(amount,(int,float)):
             raise TypeError("Amount should be NUMERIC!")
-        elif amount<0:
-            raise ValueError("Amount Should not be Negative.")
+        elif amount<=0:
+            raise ValueError("Amount should be greater than zero.")
 
     def get_balance(self):
         return self.__balance
@@ -26,35 +75,40 @@ class Account(ABC):
 
     def deposit(self,amount):
         self._validate_amount(amount)
-        new_balance=self.get_balance()+amount
-        self.set_balance(new_balance)
+        self.set_balance(self.get_balance()+amount)
         self.transactions.append(f"+₹{amount} Deposited")
-        print(f"{self.name} (Account No: {self.account_number}) deposited ₹{amount} successfully. Current Balance: ₹{self.get_balance()}")
+        print(
+            f"\n₹{amount} deposited successfully into {self.name}'s account "
+            f"(Account No: {self.account_number}). "
+            f"Current Balance: ₹{self.get_balance():.2f}."
+        )
 
     @abstractmethod
     def withdraw(self,amount):
         pass
 
-    def display(self):
-        print(f"\nAccount Number: {self.account_number}")
-        print(f"Account Holder: {self.name}")
-        print(f"Account Type: {self.__class__.__name__}")
-        print(f"Balance: ₹{self.get_balance()}")
-
     def _deduct(self,amount):
-        new_balance=self.get_balance()-amount
-        self.set_balance(new_balance)
+        self.set_balance(self.get_balance()-amount)
+
+    def display(self):
+        print(f"\nBank Name      : {Bank.bank_name}")
+        print(f"Branch Name    : {self.branch_name}")
+        print(f"Account Number : {self.account_number}")
+        print(f"Account Holder : {self.name}")
+        print(f"Account Type   : {self.__class__.__name__}")
+        print(f"Balance        : ₹{self.get_balance():.2f}")
+
 
     def show_transactions(self):
         print(f"\nTransaction History")
-        print(f"Account Number: {self.account_number}")
-        print(f"Account Holder: {self.name}")
-        print(f"Account Type: {self.__class__.__name__}")
-        if not self.transactions:
-            print("No transactions found")
-        else:
-            for transaction in self.transactions:
-                print(transaction)
+        print(f"Bank Name      : {Bank.bank_name}")
+        print(f"Branch Name    : {self.branch_name}")
+        print(f"Account Number : {self.account_number}")
+        print(f"Account Holder : {self.name}")
+        print(f"Account Type   : {self.__class__.__name__}")
+        print(f"Transactions:")
+        for transaction in self.transactions:
+            print(transaction)
 
 
 class SavingsAccount(Account):
@@ -62,13 +116,17 @@ class SavingsAccount(Account):
     def withdraw(self,amount):
         self._validate_amount(amount)
         if amount>self.get_balance():
-            print("Insufficient Balance")
+            print(f"\nInsufficient Balance.")
         elif amount>40000:
-            print("Daily Limit Exceeded")
+            print(f"\nDaily Limit Exceeded.")
         else:
             self._deduct(amount)
             self.transactions.append(f"-₹{amount} Withdrawn")
-            print(f"Withdrawal of ₹{amount} from {self.name}'s account (Account No: {self.account_number}) was successful. Current Balance: ₹{self.get_balance()}")
+            print(
+                f"\nWithdrawal of ₹{amount} from {self.name}'s account "
+                f"(Account No: {self.account_number}) was successful. "
+                f"Current Balance: ₹{self.get_balance():.2f}."
+            )
 
 
 class CurrentAccount(Account):
@@ -76,38 +134,29 @@ class CurrentAccount(Account):
     def withdraw(self, amount):
         self._validate_amount(amount)
         if amount > self.get_balance():
-            print("Insufficient Balance")
+            print("\nInsufficient Balance.")
         else:
             self._deduct(amount)
             self.transactions.append(f"-₹{amount} Withdrawn")
-            print(f"Withdrawal of ₹{amount} from {self.name}'s account (Account No: {self.account_number}) was successful. Current Balance: ₹{self.get_balance()}")
+            print(
+                f"\nWithdrawal of ₹{amount} from {self.name}'s account "
+                f"(Account No: {self.account_number}) was successful. "
+                f"Current Balance: ₹{self.get_balance():.2f}."
+            )
 
-class Bank:
-
-    def __init__(self,total):
-        self.total=total
-
-    def __add__(self,other):
-        return Bank(self.total+other.total)
-
-    def display_total(self):
-        print(f"\nTotal Bank Balance: ₹{self.total}")
-
-acc1=SavingsAccount("Sai",10000)
-acc2=CurrentAccount("Ishaan",20000)
-
+Bank.display_all_accounts()
+Bank.display_total_balance()
+acc1=SavingsAccount("Atharv","Mot",5000)
+acc2=CurrentAccount("Nancy","Hyt",9000)
+acc1.withdraw(600)
+acc2.deposit(5000)
+acc2.withdraw(16000)
 acc1.display()
-print(acc2.get_balance())
-acc1.deposit(5000)
-acc1.withdraw(2000)
-acc2.withdraw(2000)
-acc2.display()
-
-bank1=Bank(acc1.get_balance())
-bank2=Bank(acc2.get_balance())
-bank1.display_total()
-
-total=bank1+bank2
-total.display_total()
-
 acc2.show_transactions()
+Bank.display_all_accounts()
+Bank.display_total_balance()
+
+
+
+
+
